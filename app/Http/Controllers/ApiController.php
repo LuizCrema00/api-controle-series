8ae86cd446de609aa4f0dcfc51b06e6978103141
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-05-16 21:17:55 -0300

    Adicao SeriesForm Request, Repositories e SeriesRepositoryProvider

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
+use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository) 
+    {
+
+    }
     //
+    public function index() {
+        return Series::all();
+    }
+    public function store(SeriesFormRequest $request) {
+        return response()
+            ->json($this->seriesRepository->add($request), 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/series', [ApiController::class, 'create']);
-Route::post('/series', [ApiController::class, 'store']);
+Route::apiResource('/series', ApiController::class);
