f03713caec53a1b8841a6fadb373aafad877c179
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-05-21 14:02:49 -0300

    Adicao de seeder para user, rota de login para autenticacao de token com sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\ApiController;
 use App\Models\Episode;
 use App\Models\Series;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,17 +23,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/series', ApiController::class);
-Route::get('/series/{series}/seasons', function (Series $series) {
-    return $series->seasons;
-});
-Route::get('/series/{series}/episodes', function (Series $series) {
-    return $series->episodes;
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', ApiController::class);
+    Route::get('/series/{series}/seasons', function (Series $series) {
+        return $series->seasons;
+    });
+    Route::get('/series/{series}/episodes', function (Series $series) {
+        return $series->episodes;
+    });
 
-Route::patch('/episodes/{episode}', function (Episode $episode, Request $request) {
-    $episode->watched = $request->watched;
-    $episode->save();
+    Route::patch('/episodes/{episode}', function (Episode $episode, Request $request) {
+        $episode->watched = $request->watched;
+        $episode->save();
 
     return $episode;
+    });
+});
+
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only(['email', 'password']);
+    if(Auth::attempt($credentials) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+
+    $user = Auth::user();
+    $token = $user->createToken('token');
+
+    return response()->json($token->plainTextToken);
 });
